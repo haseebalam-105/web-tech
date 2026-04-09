a2035a9d40cc5fce35da2e5ad83eec8b06f9cbfb
Haseeb Alam <haseeb.rajpoot9931@gmail.com>
2026-04-09 13:03:23 +0500

    Labtask-2 filemodification

--- Labtask-2/server.js
+++ Labtask-2/server.js
@@ -1,19 +1,13 @@
-let express = require("express");
-let app = express();
+const express = require("express");
+const app = express();
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
-app.get("/contact-us", function (req, res) {
-  return res.render("contact-us");
+app.get("/", (req, res) => {
+    res.render("index");
 });
 
-app.get("/", function (req, res) {
-  return res.render("index");
+app.listen(3000, () => {
+    console.log("Server running on http://localhost:3000");
 });
-
-app.listen(3000, function () {
-  console.log("Server Started at localhost:3000");
-});
-
-console.log("Console from server.js file");
