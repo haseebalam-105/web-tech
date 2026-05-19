25eacd6b01b1f7b4825339d765fd4ad9b46b6fa4
Haseeb Alam <haseeb.rajpoot9931@gmail.com>
2026-05-19 07:35:06 +0500

    Added LabTask4

--- LabTask4/createAdmin.js
+++ LabTask4/createAdmin.js
@@ -7,7 +7,7 @@ const MONGO_URI = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/assignment
 
 async function run() {
   await mongoose.connect(MONGO_URI);
-  const email = 'admin@royaltag.com';
+  const email = 'admin@gmail.com';
   const exists = await User.findOne({ email });
   if (exists) { console.log('Admin already exists:', email); return mongoose.disconnect(); }
   await User.create({
@@ -16,8 +16,8 @@ async function run() {
     password: await bcrypt.hash('admin1234', 10),
     role:     'admin',
   });
-  console.log('✅ Admin created:', email, '/ password: admin1234');
-  console.log('⚠️  Change the password after first login!');
+  console.log(' Admin created:', email, '/ password: admin1234');
+  console.log('  Change the password after first login!');
   mongoose.disconnect();
 }
 run().catch(console.error);
--- LabTask4/seed/seed.js
+++ LabTask4/seed/seed.js
@@ -12,17 +12,30 @@ const seedProducts = [];
 for (let i = 1; i <= 30; i++) {
     const category = categories[i % categories.length];
     
-    let name = "";
-    if (category === "Polo") name = `Signature Polo ${i}`;
-    if (category === "Casual") name = `Heritage Casual Shirt ${i}`;
-    if (category === "Festive") name = `Prestige Shalwar Kameez ${i}`;
+    let image = "";
+    if (category === "Polo") {
+        name = `Signature Polo ${i}`;
+        const poloImages = ["product1.webp", "product2.webp", "product3.webp", "product4.webp", "product5.webp", "product6.jpg", "product7.jpg", "product8.webp"];
+        image = `/images/${poloImages[i % poloImages.length]}`;
+    }
+    if (category === "Casual") {
+        name = `Heritage Casual Shirt ${i}`;
+        const casualImages = ["causal-product1.webp", "causal-product2.webp", "causal-product3.webp", "causal-product4.webp", "causal-product5.webp", "causal-product6.webp", "causal-product7.webp", "causal-product8.webp", "causal-product9.webp", "causal-product10.webp"];
+        image = `/images/${casualImages[i % casualImages.length]}`;
+    }
+    if (category === "Festive") {
+        name = `Prestige Shalwar Kameez ${i}`;
+        const festiveImages = ["festive-product1.webp", "festive-product2.webp", "festive-product3.webp", "festive-product4.jpg", "festive-product5.webp", "festive-product6.webp", "festive-product7.jpg", "festive-product8.webp", "festive-product9.webp", "festive-product10.jpg"];
+        image = `/images/${festiveImages[i % festiveImages.length]}`;
+    }
 
     seedProducts.push({
         name: name,
         price: Math.floor(Math.random() * 5000) + 3000, 
         category: category,
         rating: (Math.random() * 1 + 4).toFixed(1),
-        stock: Math.floor(Math.random() * 8) + 1 
+        stock: Math.floor(Math.random() * 8) + 1,
+        image: image
     });
 }
 
